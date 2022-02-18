564b6f74a0fe34602545d91d6118a7633baf074a
Dylan Phelan <dtphelan1@gmail.com>
2022-02-17 19:05:27 -0500

    set up deploy workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    // Recognize Jest global variables like `test` and `expect`
     'jest/globals': true,
   },
   extends: ['eslint:recommended', 'airbnb', 'plugin:react/recommended', 'prettier'],
