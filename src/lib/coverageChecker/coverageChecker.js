52f702507c6611ceb57ff2cb66712962edd462a8
Dylan Phelan <dtphelan1@gmail.com>
2022-04-08 16:54:53 -0400

    remove unnecessary factory

--- src/lib/coverageChecker/coverageChecker.js
+++ src/lib/coverageChecker/coverageChecker.js
@@ -1,11 +1,5 @@
-const { log } = require('../logger');
-const { patientId, outcomeId } = require('../coverageSectionIds');
-const coverageCheckerFactory = require('./coverageCheckerFactory');
-
-// The sections against which we should compute coverage statistics;
-// These need to be sectionIds as defined by coverageSectionIds and
-// These need to be sectionIds supported by the coverageCheckerFactory
-const SECTIONS = [patientId, outcomeId];
+const { getPatientCoverage } = require('./patientCoverage');
+const { getOutcomeCoverage } = require('./outcomeCoverage');
 
 /**
  * Computes mCODE coverage statistics for a FHIR bundle
@@ -13,11 +7,7 @@ const SECTIONS = [patientId, outcomeId];
  * @returns Array of coverage statistics for each bundle
  */
 function coverageChecker(bundle) {
-  return SECTIONS.map((section) => {
-    log(coverageCheckerFactory);
-    const sectionCoverageChecker = coverageCheckerFactory(section);
-    return sectionCoverageChecker(bundle);
-  });
+  return [getPatientCoverage(bundle), getOutcomeCoverage(bundle)];
 }
 
 export default coverageChecker;
