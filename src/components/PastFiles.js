003fbb57bbcc1d84b76dac6d3bf2f0b23d961471
julianxcarter <juliancarter55@gmail.com>
2022-10-28 13:10:17 -0400

    JSON preview

--- src/components/PastFiles.js
+++ src/components/PastFiles.js
@@ -1,4 +1,6 @@
 import { Icon } from '@iconify/react';
+import ReactJson from 'react-json-view';
+import { useState } from 'react';
 
 const files = [
   {
@@ -28,40 +30,70 @@ const files = [
 ];
 
 function PastFiles() {
+  const [jsonViewIndexes, setJsonViewIndexes] = useState(new Set());
+
+  function toggleView(index) {
+    const modifiedViewIndexes = new Set(jsonViewIndexes);
+    if (jsonViewIndexes.has(index)) {
+      modifiedViewIndexes.delete(index);
+    } else {
+      modifiedViewIndexes.add(index);
+    }
+    setJsonViewIndexes(modifiedViewIndexes);
+  }
+
   return (
     <>
       <h1 style={{ fontSize: '32px', fontWeight: 'bold' }}>Past Files</h1>
-      <p style={{ fontSize: '12px' }}>These files are included within the longitudinal data</p>
+      <p style={{ fontSize: '15px' }}>These files are included within the longitudinal data</p>
       <table style={{ backgroundColor: '#ECECEC' }} className="w-full table-fixed text-left rounded shadow-md">
         <thead className="text-gray-500 font-thin rounded">
           <tr className="">
-            <th className="px-6 font-normal">Name</th>
-            <th className="font-normal">File size</th>
-            <th className="font-normal">Date added</th>
-            {/* <th /> */}
+            <th scope="col" className="px-6 font-normal py-3">
+              Name
+            </th>
+            <th scope="col" className="font-normal py-3">
+              File size
+            </th>
+            <th scope="col" className="font-normal py-3">
+              Date added
+            </th>
           </tr>
         </thead>
         <tbody>
           {files && files.length > 0 ? (
-            files.map((file) => (
-              <tr key={file.name} className="bg-white border-b" style={{ fontSize: '15px' }}>
-                <td className="px-6">
-                  <Icon icon="bi:file-earmark-code" style={{ color: 'gray' }} />
-                  {file.name}
-                </td>
-                <td className="">{file.size}</td>
-                <td className="flex flex-row justify-between">
-                  <div>{file.dateAdded}</div>
-                  <div className="text-xs">
-                    <button type="button" className="font-bold text-blue-500">
-                      View
-                    </button>
-                    <button className="px-6 font-bold text-red-500" type="button">
-                      Delete
-                    </button>
-                  </div>
-                </td>
-              </tr>
+            files.map((file, index) => (
+              <>
+                <tr key={file.name} className="bg-white border-b" style={{ fontSize: '15px' }}>
+                  <td className="px-6 text-base">
+                    <Icon
+                      icon="bi:file-earmark-code"
+                      className="text-4xl mr-2 my-2"
+                      style={{ color: 'gray', display: 'inline-block' }}
+                    />
+                    {file.name}
+                  </td>
+                  <td className="">{file.size}</td>
+                  <td className="flex flex-row justify-between items-center my-2 h-full">
+                    <div>{file.dateAdded}</div>
+                    <div className="text-xs">
+                      <button onClick={() => toggleView(index)} type="button" className="font-bold text-blue-500">
+                        {jsonViewIndexes.has(index) ? 'Hide' : 'View'}
+                      </button>
+                      <button className="px-6 font-bold text-red-500" type="button">
+                        Delete
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+                {jsonViewIndexes.has(index) ? (
+                  <tr>
+                    <td className="px-6" colSpan="3">
+                      <ReactJson src={file.body} collapsed={4} />
+                    </td>
+                  </tr>
+                ) : undefined}
+              </>
             ))
           ) : (
             <td colSpan="3" className="text-center">
