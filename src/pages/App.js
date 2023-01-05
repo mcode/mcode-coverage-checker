7982fd8262668d30df5f6f6ace7f2e6a1dac4b99
Dylan Phelan <dtphelan1@gmail.com>
2023-01-05 12:47:19 -0500

    created linechart component and using some example data for demonstration

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,11 +5,57 @@ import testbundle from '../data/fullBundle.json';
 import MainSvg from '../components/oldSvg/MainSvg';
 import Rankings from '../components/Rankings';
 import styles from '../styles/App.module.css';
+import LineChart from '../components/LineChart';
 
 function App() {
   const coverageData = coverageChecker(testbundle);
   // TODO: Remove this when website is in 1.0
   log(coverageData);
+  const dataStatic = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
   return (
     <>
       <header className={styles['app-header']}>
@@ -20,6 +66,7 @@ function App() {
           <MainVisualization coverageData={coverageData} className={styles.app} />
           <Rankings coverageData={coverageData} />
         </div>
+        <LineChart data={dataStatic} xKey="name" yKey="uv" color="#8884d8" />
         <MainSvg />
       </div>
     </>
