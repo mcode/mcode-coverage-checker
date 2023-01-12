4deb92da6d099e459311d743e5790b505f5bba18
Alden <Aldenjohnsonbrown@gmail.com>
2023-01-11 22:17:34 -0500

    Added sparklines to the rankings section

--- src/components/Rankings.js
+++ src/components/Rankings.js
@@ -1,12 +1,65 @@
 import { useState } from 'react';
 import { getAllFieldCoveredCounts } from '../lib/coverageStats/statsUtils';
 import { sectionColors } from '../lib/coverageSectionIds';
+import Sparkline from './Sparklines';
 
 const MINNUMSHOWN = 5;
 
+<<<<<<< HEAD
 function Rankings({ coverageData, className }) {
   const fields = getAllFieldCoveredCounts(coverageData);
   const [sortFunction, setSortFunction] = useState('Ascending');
+=======
+const dataStatic = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+function Rankings({ coverageData }) {
+  const [fields, setFields] = useState(
+    getAllFieldCoveredCounts(coverageData).sort((a, b) => a.percentage - b.percentage),
+  );
+>>>>>>> Added sparklines to the rankings section
   const [numShown, setNumShown] = useState(MINNUMSHOWN);
 
   function toggleRankingsShown() {
@@ -44,6 +97,7 @@ function Rankings({ coverageData, className }) {
         <div className="h-72 overflow-y-auto">
           <table className="table-auto">
             <tbody>
+<<<<<<< HEAD
               {fields
                 .sort(SORT_FUNCTIONS[sortFunction])
                 .slice(0, numShown)
@@ -61,6 +115,25 @@ function Rankings({ coverageData, className }) {
                     <td className="text-[15px] py-1">{`${field.covered}/${field.total}`}</td>
                   </tr>
                 ))}
+=======
+              {fields.slice(0, numShown).map((field) => (
+                <tr key={[field.profile, field.name].join()}>
+                  <td className="w-5 py-1">
+                    <svg className={`${sectionColors[field.section]}`} width="5" height="40">
+                      <rect width="5" height="40" rx="1" />
+                    </svg>
+                  </td>
+                  <td className="w-full py-1">
+                    <p className="text-[15px]">{field.name}</p>
+                    <p className="text-xs text-gray-400">{field.profile}</p>
+                  </td>
+                  <td className="px-2">
+                    <Sparkline data={dataStatic} />
+                  </td>
+                  <td className="text-[15px] py-1">{`${field.covered}/${field.total}`}</td>
+                </tr>
+              ))}
+>>>>>>> Added sparklines to the rankings section
               <tr>
                 <td colSpan="3" className="text-center py-1">
                   <button onClick={() => toggleRankingsShown()} type="button">
