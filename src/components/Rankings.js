17cfdf7784b2be74868e31986a61e852c5d84b59
Alden <Aldenjohnsonbrown@gmail.com>
2023-01-11 22:17:34 -0500

    Added sparklines to the rankings section

--- src/components/Rankings.js
+++ src/components/Rankings.js
@@ -1,8 +1,54 @@
 import { useState } from 'react';
 import { getAllFieldCoveredCounts } from '../lib/coverageStats/statsUtils';
 import { sectionColors } from '../lib/coverageSectionIds';
+import Sparkline from './Sparklines';
 
 const MINNUMSHOWN = 5;
+
+const dataStatic = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 function Rankings({ coverageData }) {
   const [fields, setFields] = useState(
@@ -62,6 +108,9 @@ function Rankings({ coverageData }) {
                   <td className="w-full py-1">
                     <p className="text-[15px]">{field.name}</p>
                     <p className="text-xs text-gray-400">{field.profile}</p>
+                  </td>
+                  <td className="px-2">
+                    <Sparkline data={dataStatic} />
                   </td>
                   <td className="text-[15px] py-1">{`${field.covered}/${field.total}`}</td>
                 </tr>
