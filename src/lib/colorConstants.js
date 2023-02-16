08c280b63ea254e49b4919c9c48b6f12944e6fbc
Dylan Phelan <dtphelan1@gmail.com>
2023-02-16 16:28:40 -0500

    inital asset files added

---
+++ src/lib/colorConstants.js
@@ -0,0 +1,16 @@
+// Think of this file as an ENUM
+const colors = {
+  background: '#ececec',
+  surface: '#fff',
+  patient: '#d24200',
+  disease: '#f2b84b',
+  assessment: '#f2913d',
+  genomics: '#26c485',
+  treatment: '#04b2d9',
+  outcome: '#8a45d9',
+  link: '#2f80ed',
+  'link-icon-background': '#c8d7eb',
+  'link-tinted': '#e3e6eb',
+  'heading-grey': '#767676',
+};
+export default colors;
