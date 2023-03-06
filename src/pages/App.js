afa20eae57875c2c8d07970f00e792015abf6537
Dylan Mendelowitz <dmendelowitz@mitre.org>
2023-03-06 10:46:55 -0500

    Minor adjustments to resizing styling

--- src/pages/App.js
+++ src/pages/App.js
@@ -22,29 +22,30 @@ function App() {
   );
 
   return (
-    <div className="-mt-6 max-w-6xl">
+    <div className="-mt-6 max-w-6xl w-full">
       {/* Negative margin sized to height of fileSelect –  moves select moved up into margins, aligns titles */}
       <FileSelect files={files} onChange={changeDataSource} />
       <h1 className="font-sans font-bold text-4xl">Coverage Overview</h1>
       <p className="text-sm text-gray-600 pb-2">Select a category to analyze it in finer detail</p>
-      <div className="flex flex-row max-md:flex-wrap pb-5 gap-5 items-stretch">
+      <div className="flex flex-row max-lg:flex-wrap pb-5 gap-5 items-stretch">
         <MainVisualization
+          className="max-lg:w-full lg:w-3/5"
           coverageData={coverageData}
           selectedSection={selectedSection}
           setSelectedSection={setSelectedSection}
         />
-        <Rankings coverageData={coverageData} />
+        <Rankings className="max-lg:w-full lg:w-2/5" coverageData={coverageData} />
       </div>
       <h2 className="font-sans font-semibold text-2xl">Analysis</h2>
       <p className="text-sm text-gray-600">Fine tune your analysis through your selection of subcategories</p>
-      <div className="flex flex-row max-md:flex-wrap gap-5 items-start">
+      <div className="flex flex-row max-lg:flex-wrap gap-5 items-start">
         <SubcategoryTable
-          className="h-[500px] lg:w-1/2 max-md:grow"
+          className="h-[500px] lg:w-1/2 max-lg:w-full"
           selectedSection={selectedSection}
           coverageData={coverageData}
         />
         <Longitudinal
-          className="h-[500px] lg:w-1/2 max-md:grow"
+          className="h-[500px] lg:w-1/2 max-lg:w-full"
           selectedSection={selectedSection}
           coverageData={coverageData}
         />
