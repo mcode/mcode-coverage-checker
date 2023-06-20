a4210ecee687693f30460a9e81695aa037a187b7
Dylan Mendelowitz <dmendelowitz@mitre.org>
2023-06-20 10:49:17 -0400

    Adding settings page

---
+++ src/pages/Settings.js
@@ -0,0 +1,14 @@
+function Settings() {
+  return (
+    <div className="max-w-7xl w-full">
+      <h1 className="font-sans font-bold text-4xl">Settings</h1>
+      <hr className="m-2 h-px bg-gray-300 border-0" />
+      <h2 className="pt-2 font-sans text-3xl font-semibold">Manage Your mCODE Resources</h2>
+      <p className="text-sm text-gray-600 pb-2">
+        Select which resources you want to be included in the dashboard calculation
+      </p>
+    </div>
+  );
+}
+
+export default Settings;
